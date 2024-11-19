85499f61d70eb77b27b6b1f5b73d4cbe488a12c5
Felipe Silva Loschi <166191574+F-Loschi@users.noreply.github.com>
2024-11-19 10:48:15 -0300

    Create Exercício 10, parte 1, Haskell -  Triplicando lista.hs

---
+++ Exercício 10, parte 1, Haskell -  Triplicando uma lista.hs
@@ -0,0 +1,11 @@
+module Main where
+
+main :: IO ()
+
+exerc1 :: Int -> Int
+exerc1 x = 3 * x
+
+main = do
+  let lista = [30,29..1]
+  let listaTransformada = reverse (map exerc1 lista)
+  print (maximum listaTransformada)
